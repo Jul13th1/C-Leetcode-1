159b8742a606cc4783129dc33d5bcfda32deb0c3
ke <92428508@qq.com>
2024-11-11 16:36:16 +0800

    翻转字符串1

--- 1.cpp
+++ 1.cpp
@@ -1,81 +1,27 @@
 /*
-有效字母异位词
-两个字符串 由相同的字母组成 顺序不同
+反转字符串
+示例 1： 
+输⼊：["h","e","l","l","o"] 
+输出：["o","l","l","e","h"]
 */
-
+// swap两种交换数值
+/*
+第一种：引用或者指针
+int tmp = s[i];
+s[i] = s[j];
+s[j] = tmp;
+第二种：位运算
+s[i] ^= s[j];
+s[j] ^= s[i];
+s[i] ^= s[j];
+*/
 #include <iostream>
-#include <string>
+#include <vector>
 
-int main()
+void reverseString(std::vector<char> &s)
 {
-    std::string s1 = "hello";
-    std::string s2 = "lloeh";
-
-    int record[26] = {0};
-    for(int i = 0; i < s1.size(); i++)
+    for(int i = 0,j = s.size()-1;i < s.size()/2; i++,j--)
     {
-        record[s1[i] - 'a']++;
+        std::swap[s[i],s[j]];
     }
-    for(int i = 0; i < s2.size(); i++)
-    {
-        record[s2[i] - 'a']--;
-    }
-    for(int i = 0; i < 26; i++)
-    {
-        if(record[i] != 0)
-        {
-            return false;
-        }
-    }
-}
-
-#include <iostream>
-#include <unordered_map>
-#include <string>
-using namespace std;
-
-bool isAnagram(string s, string t) 
-{
-    // 如果两个字符串长度不同，直接返回 false
-    if (s.length() != t.length())
-    {
-        return false;
-    }
-
-    // 哈希表来统计字符频率
-    unordered_map<char, int> count_map;
-
-    // 遍历第一个字符串，增加字符的频率
-    for (char c : s) 
-    {
-        count_map[c]++;
-    }
-
-    // 遍历第二个字符串，减少字符的频率
-    for (char c : t) 
-    {
-        count_map[c]--;
-        // 如果某个字符频率为负数，说明该字符在 t 中出现次数比 s 中多，返回 false
-        if (count_map[c] < 0) 
-        {
-            return false;
-        }
-    }
-
-    // 如果所有字符的频率都相同，返回 true
-    return true;
-}
-
-int main() {
-    string s = "anagram";
-    string t = "nagaram";
-
-    if (isAnagram(s, t)) 
-    {
-        cout << "Yes, they are anagrams!" << endl;
-    } else {
-        cout << "No, they are not anagrams!" << endl;
-    }
-
-    return 0;
 }
